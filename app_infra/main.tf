b51ed5598cc8fcad3db1665af4f17be7d3236805
ReaSlyn <reaslynn@gmail.com>
2024-03-13 18:35:39 +0100

    feat(pipeline): testing stage

--- app_infra/main.tf
+++ app_infra/main.tf
@@ -126,6 +126,22 @@ resource "aws_codepipeline" "codepipeline" {
   }
 
   stage {
+    name = "Test"
+    action {
+      name             = "TestAction"
+      category         = "Test"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      input_artifacts  = ["source_output"]
+      output_artifacts = ["test_output"]
+      version          = "1"
+      configuration    = {
+        ProjectName = aws_codebuild_project.flask_app_test.name
+      }
+    }
+  }
+
+  stage {
     name = "Deploy"
 
     action {
@@ -278,4 +294,29 @@ resource "aws_iam_role_policy" "codebuild_policy" {
       } 
     ]
   })
+}
+
+
+resource "aws_codebuild_project" "flask_app_test" {
+  name          = "flask-app-test"
+  description   = "Runs tests for the flask application"
+  service_role  = aws_iam_role.codebuild_role.arn
+  build_timeout = "15"
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+  environment {
+    compute_type    = "BUILD_GENERAL1_SMALL"
+    image           = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
+    type            = "LINUX_CONTAINER"
+    privileged_mode = true
+    environment_variable {
+      name  = "TEST_ENV_VAR"
+      value = "value"
+    }
+  }
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = "testspec.yml"
+  }
 }
